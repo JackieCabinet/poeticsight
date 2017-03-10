e808a9a763668d11d2c5183b5fdc5cd10e13aba6
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-03-09 20:44:19 -0800

    initial set up

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+
+app.use( morgan() );
+app.use( bp.json() );
+app.use(bp.urlencoded({ extended: true }));
+app.use('/', express.static('../'));
+
+app.set('port', (process.env.PORT || 3001));
+app.listen(app.get('port'), function(){
+	console.log('API Server started: http://localhost:' + app.get('port') + '/');
+})
