ac8f6e840baf4863839eb9d3414dbc322ae29840
Jackie Feiler <your_jacqueline.elana@gmail.com>
2017-05-10 18:19:36 -0700

    first version form

--- dev/containers/poem-form.jsx
+++ dev/containers/poem-form.jsx
@@ -5,7 +5,23 @@ import { connect } from 'react-redux';
 export default class PoemForm extends Component {
 	render() {
 		return (
-			<div>form</div>
+			<div>
+				<form className="form-horizontal">
+				  <div className="form-group">
+				    <label for="line1">Line 1:</label>
+				    <input type="text" className="form-control" id="line1"></input>
+				  </div>
+				  <div className="form-group">
+				    <label for="line2">Line 2:</label>
+				    <input type="text" className="form-control" id="line2"></input>
+				  </div>
+				  <div clasNames="form-group">
+				    <label for="line3">Line 3:</label>
+				    <input type="text" className="form-control" id="line3"></input>
+				  </div>
+				  <button type="submit" className="btn btn-default">Submit</button>
+				</form>
+			</div>
 		)
 	}
 }
